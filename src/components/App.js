66b6503c6fd13881b7d18e6fb38d45e351efad5b
andriyPro100 <andriy.anatolich@gmail.com>
2021-02-01 10:53:38 +0200

    init project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../styles/App.module.css'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Car</h1>
+            </div>
+        )
+    }
+}
